1bc07a1a49e9248783fdc2522912899b6a0534a4
HoangDang <dang.hoangthe2209@gmail.com>
2024-08-10 23:28:56 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL =  "https://be-english-web.onrender.com/"   //'http://localhost:3500';
+const BASE_URL =  "https://be-english-web.onrender.com/3500"   //'http://localhost:3500';
 
 export default axios.create({
     baseURL: BASE_URL
