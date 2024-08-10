75f7794e9386338f81b9b336dfa0ad8a8411c0f1
HoangDang <dang.hoangthe2209@gmail.com>
2024-08-11 00:34:49 +0700

    Update Navbar.js

--- src/components/admin/Navbar.js
+++ src/components/admin/Navbar.js
@@ -13,7 +13,9 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg bg-info-subtle shadow-sm rounded">
                 <div className="container-fluid">
-                    <p>Quản Lý</p>
+                    <Link className="navbar-brand" className="text-primary display-6 fw-medium">
+                    Quản Lý
+                    </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
