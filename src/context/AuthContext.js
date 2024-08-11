163dde53286b5af2240b251bd3469c7683ea6650
HoangDang <dang.hoangthe2209@gmail.com>
2024-08-11 16:47:59 +0700

    Update AuthContext.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -4,32 +4,51 @@ import useAxiosFetchUser from "../hooks/useAxiosFetchUser";
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-    // Xác thực đăng nhập
-    const [auth, setAuth] = useState('');
+  // Xác thực đăng nhập
+  const [auth, setAuth] = useState("");
 
-    const [userId, setUserId] = useLocalStorage('userId', "");
-    const [token, setToken] = useLocalStorage('token', "");
-    const [isLoggedIn, setIsLoggedIn] = useLocalStorage('isLoggedIn', false);
-    const [userAvatar, setUserAvatar] = useLocalStorage('userAvatar', "");
+  const [userId, setUserId] = useLocalStorage("userId", "");
+  const [token, setToken] = useLocalStorage("token", "");
+  const [isLoggedIn, setIsLoggedIn] = useLocalStorage("isLoggedIn", false);
+  const [userAvatar, setUserAvatar] = useLocalStorage("userAvatar", "");
+  const [roles, setRoles] = useLocalStorage("roles", "");
 
-    const [userLogin, setUserLogin] = useState('');
-    const { data: dataUser, fetchError: fetchErrorUser, isLoading: isLoadingUser } = useAxiosFetchUser(`/users/${userId}`);
+  const [userLogin, setUserLogin] = useState("");
+  const {
+    data: dataUser,
+    fetchError: fetchErrorUser,
+    isLoading: isLoadingUser,
+  } = useAxiosFetchUser(`/users/${userId}`);
 
-    useEffect(() => {
-        if (userId !== "") {
-            setUserLogin(dataUser);
-        }
-    }, [userId, dataUser])
+  useEffect(() => {
+    if (userId !== "") {
+      setUserLogin(dataUser);
+    }
+  }, [userId, dataUser]);
 
-    return (
-        <AuthContext.Provider value={{
-            auth, setAuth, userLogin, fetchErrorUser, isLoadingUser,
-            token, setToken, setUserId, isLoggedIn, setIsLoggedIn,
-            userAvatar, setUserAvatar,
-        }}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  return (
+    <AuthContext.Provider
+      value={{
+        auth,
+        setAuth,
+        userLogin,
+        fetchErrorUser,
+        isLoadingUser,
+        token,
+        setToken,
+        setUserId,
+        isLoggedIn,
+        setIsLoggedIn,
+        userAvatar,
+        setUserAvatar,
+        roles,
+        setRoles,
+        userId,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
